06dc06cb403c900a93fbb8ec9e1292e43c9e76ab
Nick Marcopoli <nick@marcopo.li>
2019-03-17 23:33:03 -0700

    lazy loading to speed up initial load by 7x

---
+++ assets/js/lazy.js
@@ -0,0 +1,39 @@
+// Lazy Loading
+
+// Make sure things are loaded if a user goes directly to a page
+window.onload = function(){
+    switch (window.location.hash) {
+        case "#contact":
+            showForm();
+            break;
+        case "#work":
+            showWork();
+            break;
+    }
+}
+
+// Load Google Form
+function showForm() {
+    forms = document.getElementsByClassName("contactForm");
+    if (forms.length === 0) {
+        contactForm = document.createElement("iframe");
+        contactForm.setAttribute("src", "https://docs.google.com/forms/u/1/d/e/1FAIpQLSdEmj2NkIKR9CaZYfr1UB2g_92s1ds0A99B4kxZP3mrr1cphQ/viewform?embedded=true");
+        contactForm.setAttribute("width", "100%");
+        contactForm.setAttribute("height", "1000");
+        contactForm.setAttribute("class", "contactForm");
+        document.getElementById("docsContactForm").appendChild(contactForm);
+    }
+};
+
+// Load work videos and pictures
+function showWork() {
+    resources = document.getElementsByClassName("workResource");
+    for(let element of resources){
+        // All resources to be loaded lazily have src replaced with lsrc
+        realSrc = element.getAttribute("lsrc");
+        if(realSrc != null){
+            element.setAttribute("src", realSrc);
+            element.removeAttribute("lsrc");
+        }
+    };
+}
